1861f70346c95108811160a452d2b05e5773717a
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2024-02-28 09:40:16 -0700

    this is all I got chief

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
-// index.js
-
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  const image = document.querySelector("#ramen-menu img")
+  const description = document.querySelector("#ramen-detail")
+  image.addEventListener('click', () => {
+    const ramName = document.querySelector(".name")
+    const restaurantName = document.querySelector(".restaurant")
+    ramName.textContent = ramen.name
+    restaurantName.textContent = ramen.restaurant
+    description.append(ramName, restaurantName)
+  })
 };
 
 const addSubmitListener = () => {
-  // Add code
+  const submit = document.querySelector("#submit-button")
+  submit.addEventListener('click', 'submit', () => {
+    const ramNew = document.querySelector("#ramen-menu")
+  })
 }
 
 const displayRamens = () => {
-  // Add code
+  const ramenMenu = document.querySelector("#ramen-menu");
+  return fetch("http://localhost:3000/ramens")
+    .then(r => r.json())
+    .then(data => {
+      const images = data.images
+      images.forEach(images => {
+        const createImg = document.createElement("img");
+        createImg.src = images.image;
+        ramenMenu.append(createImg)
+      });
+    });
 };
 
+
 const main = () => {
+  document.addEventListener('DOMContentLoaded')
+  displayRamens()
+  addSubmitListener()
   // Invoke displayRamens here
   // Invoke addSubmitListener here
 }
 
 main()
 
-// Export functions for testing
 export {
   displayRamens,
   addSubmitListener,
